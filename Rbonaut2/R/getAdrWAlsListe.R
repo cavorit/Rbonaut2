95d618729d1b422f32a52e48f53ff915378f9603
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2015-12-04 11:48:52 +0100

    roxygen2 initiiert

--- Rbonaut2/R/getAdrWAlsListe.R
+++ Rbonaut2/R/getAdrWAlsListe.R
@@ -1,14 +1,14 @@
 #' @author Harald Fiedler
 #' @description adrW-Einträge als numerische Liste
-#' @detail Aus c("{1, 2, 3, 4}", "{11, 12, 13, 14}") mach mache list(c(1, 2, 3, 4), c(11, 12, 13, 14))
+#' @details Aus c("{1, 2, 3, 4}", "{11, 12, 13, 14}") mach mache list(c(1, 2, 3, 4), c(11, 12, 13, 14))
 #' @title getAdrWAlsListe
-#' @param adrW character 
+#' @param adrW character
 #' @return list mit numerischen Elementen
 
 getAdrWAlsListe <- function(adrW){
   VektorLaenge <- length(adrW)
   WortLaenge <- nchar(adrW)
-  
+
   OhneGeschweifteKlammer <- NULL
   for (i in 1:VektorLaenge){
     OhneGeschweifteKlammer <- c(OhneGeschweifteKlammer, substr(adrW[i], 2, WortLaenge[i]-1))
