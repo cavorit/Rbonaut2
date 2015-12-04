f2fea0d37589a69e8efb849820c6dd9e8f7c9777
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2015-12-04 14:04:48 +0100

    docs with example

--- Rbonaut2/R/getSessionTimeStamp.R
+++ Rbonaut2/R/getSessionTimeStamp.R
@@ -1,9 +1,11 @@
 #' @author Harald Fiedler
-#' @description ermittelt Sessionstart
+#' @description Hilfsfunktion von SQL2DF(): ermittelt Sessionstart
 #' @details Macht aus 2015-08-27 18:59:25.328383+02 den String 18:59:25
 #' @title getSessionTimeStamp
 #' @param DatumString String, etwa "2015-08-27 18:59:25.328383+02"
 #' @return Ein String, etwas "18:59:25"
+#' @examples
+#' DatumString <- c("2015-08-27 18:59:25.328383+02", "2015-08-27 18:59:25.328383+02", "2015-08-27 18:59:25.328383+02")
 
 getSessionTimeStamp <- function(DatumString){
   # c("2015-08-27 18:59:25.328383+02", "2015-08-27 18:59:25.328383+02", "2015-08-27 18:59:25.328383+02")
@@ -22,4 +24,4 @@ getSessionTimeStamp <- function(DatumString){
   return(Erg)
 }
 
-#DatumString <- c("2015-08-27 18:59:25.328383+02", "2015-08-27 18:59:25.328383+02", "2015-08-27 18:59:25.328383+02")
+
