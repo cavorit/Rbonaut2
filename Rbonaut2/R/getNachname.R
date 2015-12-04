4ff182c52cb70a08f262c85dbe51414ded2be740
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2015-12-04 13:25:03 +0100

    nun sind alle Funktionen drin, die ich im Zusammenhang mit RicoWehrle entwickelt habe

---
+++ Rbonaut2/R/getNachname.R
@@ -0,0 +1,23 @@
+#' @author Harald Fiedler
+#' @description Gibt aus einem Spielername den Vornamen
+#' @details Spielernamen können in SQL-Abfragen des FBN beispielsweise "Dogan, Isa" sein. Es wird "Isa" zurückgegeben.
+#' @title getNachname
+#' @param Spielername character Vektor von beliebiger Länge
+#' @return character Vektor der gleichen Länge wie der an die Funktion übergebene Vektor
+
+getNachname <- function(Spielername){
+  Erg <- rep(NA, times=length(Spielername))    
+  Nachname <- NULL
+  for (i in strsplit(Spielername, split = ",")){
+    Nachname <- c(Nachname, i[1])
+  }
+  Wo <- istFormatNachnameKommaVorname(Spielername)
+  Erg[Wo] <- Nachname[Wo]
+  Erg <- gsub(Erg, pattern = " ", replacement="") # nimmt Leerzeichen raus  
+  return(Erg)
+}
+
+
+Spielername <- c("Fiedler, Harald", "Mayer, Jan", "A-Team")
+getNachname(Spielername = Spielername)
+
