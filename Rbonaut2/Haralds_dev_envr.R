96d6634500ffcf04c5ab9a06c818d1811da1d0b5
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2015-12-07 14:55:47 +0100

    Testung der Probleme:
    (1.) idB muss bei einstelligem idX eine führende Null bekommen, um lexikografisch sinnvoll sortiert zu werden.
    
    (2.) AW und RW scheinen in ItemBank und SQL2DF nicht gleich berechnet zu werden, wodurch das Mergen auf der Menge AW, RW nicht gelingt.

--- Rbonaut2/Haralds_dev_envr.R
+++ Rbonaut2/Haralds_dev_envr.R
@@ -6,14 +6,9 @@ ItemBank <- Rbonaut2::readItemBank()
 Pfad <- system.file("extdata", package="Rbonaut2", "Footbonaut_Datenabfrage_RicoWehrle.csv")
 SQL <- read.csv2(file=Pfad, sep = ",", stringsAsFactors = FALSE, encoding = "utf8")
 DF <- SQL2DF(SQL=SQL)
-#DF$adrM <- as.integer(DF$adrM)
-#DF$adrW <- as.integer(DF$adrW)
-#DF$AW <- as.integer(DF$AW)
-#DF$RW <- as.integer(DF$RW)
-
 
 
 M <- merge(x=DF, y=ItemBank[2:31,], all.x=TRUE, by=c("adrM", "adrW"))
 View(M)
 
-
+SessionA <- M[M$idS=="FBN-Hoffenheim-2015-08-27-13:29:38", c("adrM", "adrW", "idX", "AW.x", "AW.y", "RW.x", "RW.y", "ItemID")]
