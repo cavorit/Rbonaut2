e7ff85644b53f427a8485d2ad4bf028b2b1ebf9e
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2015-12-10 17:25:39 +0100

    playedAngle() begonnen

---
+++ Rbonaut2/R/playedAngle.R
@@ -0,0 +1,42 @@
+#' @author Harald Fiedler
+#' @details Gibt den Winkel zwischen zwei FBN-Adressen
+#' @description Winkel zwischen zwei Adressen
+#' @param A numeric Adressen der Ausgangsfelder
+#' @param B numeric Adressen der Zielfelder
+#' @param Clockwise boolean der angibt, ob die Winkel im Uhrzeigersinn oder im mathematisch positiven Sinn gemessen werden. Default ist der mathematisch positive Sinn.
+#' @return numeric mit Winkel aus 0:180
+
+
+playedAngle <- function(adrA, adrB){
+  if(any(adrA>72) | any(adrA<1) | any(adrB>72) | any(adrB<1)){stop("Adressen des FBN sind in 1:72")}
+
+  addiereImRing <- function (a, b){
+    if (a <= 0 | b <= 0)
+      warning("a und b sollten positiv sein. Nutze subtrahiereImRing() statt dessen.")
+    if (a == 72 & b == 72) {
+      return(72)
+    }
+    else {
+      return((a + b)%%72)
+    }
+  }
+
+  subtrahiereImRing <- function (a, b)
+  {
+    if (a <= 0 | b <= 0)
+      warning("a und b sollten positiv sein")
+    if (a > b)
+      return(a - b)
+    if (a == b)
+      return(0)
+    if (a < b)
+      return(a - b + 72)
+  }
+
+  LotAdrA <- adrA+adrA%%2
+  LotAdrB <- adrB+adrB%%2
+
+  Erg <- (LotAdrA - LotAdrB) * 10
+  return(Erg)
+}
+
