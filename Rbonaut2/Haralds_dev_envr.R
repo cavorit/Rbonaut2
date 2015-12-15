45679b642068aedd65f3ca0058c1556f6942d3b1
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2015-12-15 17:53:49 +0100

    minor changes

--- Rbonaut2/Haralds_dev_envr.R
+++ Rbonaut2/Haralds_dev_envr.R
@@ -11,6 +11,7 @@ library(Rbonaut2)
 
 # 4. Inder
 Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/4_Inder/SQLmanuelleQeuryVonCgoalBereitgestelltAm2015-12-14Von2015-09-30.csv"
+isUTF8(file=Pfad)
 SQL <- read.csv(file=Pfad, sep=";", header=TRUE, encoding="utf8", stringsAsFactors = FALSE)
 #View(SQL)
 DF <- SQL2DF(SQL = SQL)
