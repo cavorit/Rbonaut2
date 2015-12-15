e8d56988deaf1cb9f210db531a120ccf73869262
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2015-12-15 17:42:45 +0100

    minor bugfix

--- Rbonaut2/R/isUTF8.R
+++ Rbonaut2/R/isUTF8.R
@@ -3,6 +3,7 @@
 #' @description schätzt, ob UTF8 Codierung vorliegt
 #' @details Diese Funktion funktioniert nur auf Mac, wobei hierzu per *brew install moreutils* installiert sein muss. Sie gibt character(0) zurück, wenn kein utf8-nonkonformes Zeichen gefunden wurde, und ansonsten eine Liste mit Angaben zu invaliden Zeichen
 #' @param file character der Länge 1 der angibt, ob schätzungsweise UTF8 vorliegt
+#' @param echo boolean der Länge 1 sagt, ob der Systemoutput angezeigt werden soll
 #' @examples
 #' isUTF8(file="~/Desktop")
 #' @return siehe *details*
@@ -10,9 +11,10 @@
 #'
 
 
-isUTF8 <- function(file="*"){
+isUTF8 <- function(file="*", echo=TRUE){
   Befehl <- paste("isutf8", file, collapse = " ")
   SystemAntwort <- system(Befehl, intern=TRUE)
+  if (echo) print(SystemAntwort)
   Erg <- as.logical(length(SystemAntwort))
-  return(Erg)
+  return(!Erg)
 }
