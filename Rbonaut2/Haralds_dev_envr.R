f00e0d4b698392acc4f4b5706c578037d3b6b832
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2016-01-27 15:07:53 +0100

    Haralds_dev_env erstellt nun die Raschmatrix aus einem DF

--- Rbonaut2/Haralds_dev_envr.R
+++ Rbonaut2/Haralds_dev_envr.R
@@ -9,31 +9,32 @@ Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/2_Ch
 Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/4_Inder/SQLmanuelleQeuryVonCgoalBereitgestelltAm2015-12-14Von2015-09-30.csv" # harmlose warnings() bei den Namen
 # Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/5_IvanGojak_MA/RAW/SQLmanuelleQueryvonCgoalBereitgestelltAm2015-12-14von2015-12-05bis2015-12-05.csv" # Fehlermeldung wegen ; in Namen
 Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/10_AndreasKlundt_MA/SQLmanuelleQueryVonCgoalBereitgestelltAm2016-01-07Von2015-10-17Bis2015-10-17.csv"
-
 #isUTF8(Pfad)
 SQL <- read.csv(file=Pfad, sep=";", header=TRUE, encoding="utf8", stringsAsFactors = FALSE)
 #View(SQL)
 DF <- SQL2DF(SQL = SQL)
-View(DF)
-head(DF)
-########### Schritt 3: Füge mit Hilfe von Itembanken neue Testergebnisse hinzu
+#View(DF)
+#head(DF)
+########### Schritt 4: Füge mit Hilfe von Itembanken neue Testergebnisse hinzu
 ItemBank=readItemBank()
 # Merge die ItemBank
-
 F14 <- getItemICC(DF=DF, ItemBank=readItemBank())
 View(F14)
 
-########### Exkurs: erstelle eine neue ItemBank
-
-RaschMatrix <- matrix(NA, ncol=32, nrow=5)
+########### Schritt 3: erstelle eine neue ItemBank
+SessionIndex <- unique(DF$idS)
+RaschMatrix <- matrix(NA, ncol=32, nrow=length(SessionIndex))
 colnames(RaschMatrix) <- paste0("BL", gibZahlFuehrendeNullen(1:32, digits=2))
-rownames(RaschMatrix) <- 1:nrow(RaschMatrix)
+rownames(RaschMatrix) <- unique(DF$idS)
 RaschMatrix
 
-x <- NULL
-for (i in 1:nrow(DF)){
-  x <- c(x, detectItemResponse(DF[i,]))
+for (S in SessionIndex){# S <- SessionIndex[1]
+  EineSession <- DF[DF$idS==S,]
+  for (B in EineSession$idX){ # B <- EineSession$idX[1]
+    EinBall <- EineSession[EineSession$idX==B,]
+    if (is.element(EinBall$ItemID, colnames(RaschMatrix))){RaschMatrix[S, EinBall$ItemID] <- EinBall$ItemResponse}
+  }
 }
-x
 
-View(detectItemResponse(DF))
+RaschMatrix
+
