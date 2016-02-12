d69fdad1762c9ab2a1c823a4e426d230e42e7caa
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2016-02-12 14:31:30 +0100

    Toleranzen für die Walzen-Geschwindigkeiten bei der Detection der Items der neuen Baselin deutlich erhöht (von 15% auf 34%).

--- Rbonaut2/Haralds_dev_envr.R
+++ Rbonaut2/Haralds_dev_envr.R
@@ -7,21 +7,19 @@ library(Rbonaut2)
 Pfad <- system.file("extdata", package="Rbonaut2", "Footbonaut_Datenabfrage_RicoWehrle.csv")
 Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/2_ChristianMautner_PhD/RAW/SQLmanuelleQueryVonCgoalBereitgestelltAm2016-01-21Von2016-01-16Bis2016-01-16.csv"
 Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/4_Inder/SQLmanuelleQeuryVonCgoalBereitgestelltAm2015-12-14Von2015-09-30.csv" # harmlose warnings() bei den Namen
-# Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/5_IvanGojak_MA/RAW/SQLmanuelleQueryvonCgoalBereitgestelltAm2015-12-14von2015-12-05bis2015-12-05.csv" # Fehlermeldung wegen ; in Namen
+#Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/5_IvanGojak_MA/RAW/SQLmanuelleQueryvonCgoalBereitgestelltAm2015-12-14von2015-12-05bis2015-12-05.csv" # Fehlermeldung wegen ; in Namen
 Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Projekte/10_AndreasKlundt_MA/SQLmanuelleQueryVonCgoalBereitgestelltAm2016-01-07Von2015-10-17Bis2015-10-17.csv"
-Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Baseline-Abfrage2014-2015/RAW/Abfrage Oktober-November 2015/SQLmanuelleQueryVonCgoalBereitgestelltAm2016-02-09Von2015-09-28Bis2015-10-04.csv"
-SQL <- read.csv(file=Pfad, sep=";", header=TRUE, encoding="utf8", stringsAsFactors = FALSE)
-#View(SQL)
-DF <- SQL2DF(SQL = SQL)
-#View(DF)
-#head(DF)
-########### Schritt 4: Füge mit Hilfe von Itembanken neue Testergebnisse hinzu
-ItemBank=readItemBank()
-# Merge die ItemBank
-F14 <- getItemICC(DF=DF, ItemBank=readItemBank())
-View(F14)
 
-########### Schritt 3: erstelle eine neue ItemBank
+Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Baseline-Abfrage2014-2015/RAW/Abfrage Oktober-November 2015/SQLmanuelleQueryVonCgoalBereitgestelltAm2016-02-09Von2015-09-28Bis2015-10-04.csv"
+Pfad <- "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/Baseline-Abfrage2014-2015/RAW/Abfrage Oktober-November 2015/SQLmanuelleQueryVonCgoalBereitgestelltAm2016-02-09Von2015-10-05Bis2015-10-11.csv"
+
+
+
+SQL <- read.csv(file=Pfad, sep=";", header=TRUE, encoding="utf8", stringsAsFactors = FALSE)
+SQL$score <- SQL$points
+DF <- SQL2DF(SQL = SQL)
+
+########### erstelle eine neue ItemBank
 SessionIndex <- unique(DF$idS)
 RaschMatrix <- matrix(NA, ncol=32, nrow=length(SessionIndex))
 colnames(RaschMatrix) <- paste0("BL", gibZahlFuehrendeNullen(1:32, digits=2))
