90dbafa4ee17fe1f014a2490b497d6ee14dd99f5
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2016-02-18 11:50:54 +0100

    playedAngle() macht Fehler. Beginne mit Ausbesserungen

--- Rbonaut2/R/playedAngle.R
+++ Rbonaut2/R/playedAngle.R
@@ -3,22 +3,21 @@
 #' @description Winkel zwischen zwei Adressen
 #' @param A numeric Adressen der Ausgangsfelder
 #' @param B numeric Adressen der Zielfelder
-#' @param Clockwise boolean der angibt, ob die Winkel im Uhrzeigersinn oder im mathematisch positiven Sinn gemessen werden. Default ist der mathematisch positive Sinn.
-#' @return numeric mit Winkel aus 0:180
+#' @return numeric mit Winkel aus -170:180 wobei der Winkel positiv im Uhrzeigersinn gemessen wird
 #' @title playedAngle
 #' @examples
 #' adrA=10
 #' adrB=18
 #' plotFBN()
-#' playedAngle(adrA=adrA, adrB=adrB, Clockwise = FALSE)
+#' playedAngle(adrA=adrA, adrB=adrB)
 
 playedAngle <- function(adrA, adrB, Clockwise = FALSE){
-  if(any(adrA>72) | any(adrA<1) | any(adrB>72) | any(adrB<1)){stop("Adressen des FBN sind in 1:72")}
+  if(any(adrA>72) | any(adrA<1) | any(adrB>72) | any(adrB<1)){stop("Adressen des FBN müssen in 1:72 liegen")}
 
   Aground <- adrA+adrA%%2
   Bground <- adrB+adrB%%2
 
-  Erg <- ( Bground - Aground ) * 5 * (-1)**Clockwise
+  Erg <- ( Bground - Aground ) * 5
   return(Erg)
 
 }
