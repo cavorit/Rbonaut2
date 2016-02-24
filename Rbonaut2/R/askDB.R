3bdc55c25a893850820bb45c8103cc0707d754be
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2016-02-24 17:08:51 +0100

    SQL-Abfrage in Funktion askDB() (ungetestet)

--- Rbonaut2/R/askDB.R
+++ Rbonaut2/R/askDB.R
@@ -19,66 +19,66 @@ askDB <- function(Anfangsdatum, Enddatum){
   port = 5432
   con <- dbConnect(drv = drv, dbname=dbname, user=user, password=password, port=port)
 
-TXT1 <- "SELECT
-translate(webapp_player.name,';',',') AS spielername,
-webapp_player.birthday AS geboren,
-webapp_player.position AS pos,
-webapp_playedsession.start_date AS date,
-webapp_playedsession.session_template_ID AS template,
-webapp_playedshot.idx AS idx,
-webapp_playedshot.ballmachine AS adrm,
-webapp_playedshot.own_targets AS adrw,
-webapp_playedshot.opponent_targets AS adrd,
-webapp_playedshot.shot_delay AS delay,
-webapp_playedshot.speed_left AS sl,
-webapp_playedshot.speed_right AS sr,
-webapp_playedshot.vertical_angle AS va,
-webapp_playedshot.played_angle AS deg,
-webapp_playedshot.goal_hit AS goal,
-webapp_playedshot.goal_actual AS hit,
-webapp_playedshot.goal_actual_height AS flachstufe,
-webapp_playedshot.duration AS time,
-webapp_playedshot.points AS score,
-webapp_team.name AS team,
-webapp_footbonaut.name AS fbn"
+  Anfangsdatum = "2015-04-01"
+  Enddatum = "2015-04-22"
 
-TXT2 <- "FROM
-webapp_playedshot
-inner join (
-webapp_playedsession
-inner join
-webapp_sessiontemplate
-on webapp_playedsession.session_template_id = webapp_sessiontemplate.id
-)
-on webapp_playedshot.session_id = webapp_playedsession.id
-inner join(
-webapp_player
-inner join
-webapp_footbonaut
-on webapp_player.footbonaut_id = webapp_footbonaut.id
-inner join
-webapp_team
-on webapp_player.team_id = webapp_team.id
-)
-on webapp_playedshot.player_id = webapp_player.id
-"
+  Query <- paste0("
+  SELECT
+  translate(webapp_player.name,';',',') AS spielername,
+  webapp_player.birthday AS geboren,
+  webapp_player.position AS pos,
+  webapp_playedsession.start_date AS date,
+  webapp_playedsession.session_template_ID AS template,
+  webapp_playedshot.idx AS idx,
+  webapp_playedshot.ballmachine AS adrm,
+  webapp_playedshot.own_targets AS adrw,
+  webapp_playedshot.opponent_targets AS adrd,
+  webapp_playedshot.shot_delay AS delay,
+  webapp_playedshot.speed_left AS sl,
+  webapp_playedshot.speed_right AS sr,
+  webapp_playedshot.vertical_angle AS va,
+  webapp_playedshot.played_angle AS deg,
+  webapp_playedshot.goal_hit AS goal,
+  webapp_playedshot.goal_actual AS hit,
+  webapp_playedshot.goal_actual_height AS flachstufe,
+  webapp_playedshot.duration AS time,
+  webapp_playedshot.points AS score,
+  webapp_team.name AS team,
+  webapp_footbonaut.name AS fbn
 
-TXT3 <- "
-WHERE
-DATE(start_date) BETWEEN '2014-02-07' AND '2014-02-15'
-ORDER BY date asc;
-"
+  FROM
+  webapp_playedshot
+  INNER JOIN (
+  webapp_playedsession
+  INNER JOIN
+  webapp_sessiontemplate
+  ON webapp_playedsession.session_template_id = webapp_sessiontemplate.id
+  )
+  ON webapp_playedshot.session_id = webapp_playedsession.id
+  INNER JOIN(
+  webapp_player
+  INNER JOIN
+  webapp_footbonaut
+  ON webapp_player.footbonaut_id = webapp_footbonaut.id
+  INNER JOIN
+  webapp_team
+  ON webapp_player.team_id = webapp_team.id
+  )
+  ON webapp_playedshot.player_id = webapp_player.id
 
+  WHERE
+  DATE(start_date) BETWEEN '", Anfangsdatum, "' AND '", Enddatum, "' ORDER BY date ASC;")
+
+  DF <- dbGetQuery(conn=con, statement = Query)
+
+  dbDisconnect(con)
+
+  return(DF)
 }
 
 
 
 
-# dbListTables(con)
-# dbListFields(con,"webapp_team")
-# dbReadTable(con,"webapp_team")
-# dbGetQuery(con, statement = "select * from webapp_team")
-# dbGetQuery(con, statement = paste0(TXT1, TXT2, TXT3))
-#
-# dbDisconnect(con)
-#
+
+
+
