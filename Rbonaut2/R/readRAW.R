fceb8d8a1eaa0eac413abb4b1f081529ccbdb9e9
KaterynaCavorit <kateryna@zedat.fu-berlin.de>
2016-10-31 13:16:30 +0100

    KB hat jetzt eigene SandBox

--- Rbonaut2/R/readRAW.R
+++ Rbonaut2/R/readRAW.R
@@ -2,13 +2,14 @@
 #' @title readRAW
 #' @description Liest die per writeRAW() gespeicherten Dateien ein
 #' @param Dateiname character der Länge 1 mit Dateiname (ohne Endung). Der Pfad wird automatisch auf die Dropbox gesetzt, genauer in den Ordner RAW vom Ordner Hoffenheim
+#' @param Pfad CharacterString mit Pfadangabe zur Dropbox
 #' @examples
 #' Dateiname = "RAW-2015-04"
 #' head(readRAW(Dateiname = Dateiname))
 #'
 
-readRAW <- function(Dateiname){
-  path <- paste0("~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/RAW/", Dateiname, collapse = "/")
+readRAW <- function(Dateiname, Pfad = "~/Dropbox (Cavorit)/Cavorit/Forschungsprojekte/Hoffenheim/RAW/"){
+  path = paste0(Pfad, Dateiname, collapse = "/")
   load(file = path, verbose = TRUE, envir = .GlobalEnv)
 }
 
