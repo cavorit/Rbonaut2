2af2638ef3b524cf7c930dd7b54bc729df733d8e
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2016-11-09 10:49:21 +0100

    Vorbereitung auf fullRandom

--- Rbonaut2/R/adaptiv.BL16.fullRandom.modelTransform.R
+++ Rbonaut2/R/adaptiv.BL16.fullRandom.modelTransform.R
@@ -1,50 +1,78 @@
 #' @author Harald Fiedler
-#' @description das yhat-modelTransform() fuer einen randomisierten BL32-Test
-#' @details Es handelt sich hier um einen reinen Item-Sampler über den BL32-Item-Raum, der nach 16 Bällen abbricht.
-#' @param AnfrageJSON String, der in R zu einer Liste umgewandelt werden kann mit folgenden Elementen: 
-#' #\itemize{
-#'   #\item TestID: den Namen des Testformats. Beispielsweise: 'BL32'
-#'   #\item idS: die aktuelle Session-ID, beispielsweise: '002b6573-cf12-436d-bccd-0856b0bb0a25'
-#'   #\item idP: die Player-ID des jeweiligen Probanden, beispielsweise: 'fe553db4-bbde-43dd-a6a0-804b9e46c57'
-#'   #\item NamePlayer: Klarname des Spielers, beispielsweise: 'Mustermann, Tim'
-#'   #\item SessionStart: der Zeitstempel für den Sessionsstart als String im Format "JJJJ-MM-TT HH:MM:SS", beispielsweise: '2014-03-22 13:42:03'
-#'   #\item adrB: eine Liste mit den Adressen der Ballkanonen, in der Reihenfolge ihrer Aktivierung, beispielsweise: [10, 45, 28] oder bei Sessionstart eine leere Liste.
-#'   #\item adrW: eine Liste von Listen mit den aufleuchtenden Zielfeldern. Beispielsweise: '[[4, 6], [21, 22], [50, 18]]' oder bei Sessionstart '[[]]'
-#'   #\item adrCol: eine Liste von Listen mit der Farbe der Zielfelder aus [[adrW]]. Die Farben werden alphabetisch in Großbuchstaben durchnummeriert. Beispielsweise: "[['A', 'B'], ['A', 'B'], ['B', 'A']]" oder bei Sessionstart '[[]]'
-#'   #\item '[adrOut]' eine Liste mit den Adressen, wo der Ball tatsächlich raus ist. Beispielsweise: [4, 23, 40] oder bei Sessionstart []
-#'   #\item [FBt] eine Liste mit den Angaben über die Zeitdauer zwischen Ballkanoneneinwurf und Lichtschrankensignal beim Rausschießen des Balls (in Millisekunden), beispielsweise: "[2140, 2600, 8600]" oder bei Sessionstart "[]"
-#' } 
-#' @return list
+#' @description modelTransform() fuer yhat-Modell eines voll randomisierten Samplers des BL32-Testraums mit Stop nach 16 Bällen
+#' @details Es handelt sich hier um die modelTransform-Funktion für yhat-Architektur. Bei dem Dienst handelt es sich um einen reinen Item-Sampler. Solange die Session-History weniger als 16 Bälle aufweist, wird aus dem Item-Raum der BL32 ein Item gewählt.
+#' @param AnfrageJSON String, der in R zu einer Liste umgewandelt werden kann mit folgenden Elementen:
+#' \itemize{
+#'   \item TestID: den Namen des Testformats. Beispielsweise: 'BL32'
+#'   \item idS: die aktuelle Session-ID, beispielsweise: '002b6573-cf12-436d-bccd-0856b0bb0a25'
+#'   \item idP: die Player-ID des jeweiligen Probanden, beispielsweise: 'fe553db4-bbde-43dd-a6a0-804b9e46c57'
+#'   \item NamePlayer: Klarname des Spielers, beispielsweise: 'Mustermann, Tim'
+#'   \item SessionStart: der Zeitstempel für den Sessionsstart als String im Format "JJJJ-MM-TT HH:MM:SS", beispielsweise: '2014-03-22 13:42:03'
+#'   \item adrB: eine Liste mit den Adressen der Ballkanonen, in der Reihenfolge ihrer Aktivierung, beispielsweise: [10, 45, 28] oder bei Sessionstart eine leere Liste '[]'.
+#'   \item adrW: eine Liste von Listen mit den aufleuchtenden Zielfeldern. Beispielsweise: '[[4, 6], [21, 22], [50, 18]]' oder bei Sessionstart '[[]]'
+#'   \item adrCol: eine Liste von Listen mit der Farbe der Zielfelder aus [[adrW]]. Die Farben werden alphabetisch in Großbuchstaben durchnummeriert. Beispielsweise: "[['A', 'B'], ['A', 'B'], ['B', 'A']]" oder bei Sessionstart '[[]]'
+#'   \item adrOut: eine Liste mit den Adressen, wo der Ball tatsächlich raus ist. Beispielsweise: '[4, 23, 40]' oder bei Sessionstart '[]'
+#'   \item FBt: eine Liste mit den Angaben über die Zeitdauer zwischen Ballkanoneneinwurf und Lichtschrankensignal beim Rausschießen des Balls (in Millisekunden), beispielsweise: '[2140, 2600, 8600]' oder bei Sessionstart '[]'
+#' }
+#'
+#' Hier ein Beispiel für eine gültige Anfrage:
+#'
+#'
+#' testJSONrequest <- '\{\cr
+#' "TestID" : "BL32",\cr
+#' "idS" : "002b6573-cf12-436d-bccd-0856b0bb0a25",\cr
+#' "idP" : "fe553db4-bbde-43dd-a6a0-804b9e46c57",\cr
+#' "NamePlayer" : "Mustermann, Tim",\cr
+#' "SessionStart" : "2014-03-22 13:42:03",\cr
+#' "adrB" : [10, 45, 28],\cr
+#' "adrW" : [[4, 6], [21, 22], [50, 18]],\cr
+#' "adrCol" : [["A", "B"], ["A", "B"], ["B", "A"]],\cr
+#' "adrOut" : [4, 23, 40],\cr
+#' "FBt" : [2140, 2600, 8600]\cr
+#' \}'\cr
+#'
+#'
 #' @title adaptiv.BL16.fullRandom.modelTransform
+#' @return data.frame mit den oben beschriebenen Spalten.
 
 adaptiv.BL16.fullRandom.modelTransform <- function(AnfrageJSON){
-    
+
     AnfrageListe <- jsonlite::fromJSON(txt = AnfrageJSONstring)
-    
+
     # Lese die Variablen ein
-    TestID <- AnfrageListe$TestID # Die ID dieses Tests lautet "adaptiv.BL16.fullRandom" 
+    TestID <- AnfrageListe$TestID # Die ID dieses Tests lautet "adaptiv.BL16.fullRandom"
     idS <- AnfrageListe$idS # ID der Session
     NamePlayer <- AnfrageListe$NamePlayer # Name als ein CharacterString
-    SessionStart <- as.Date(AnfrageListe$SessionStart) # Datum Stunde Sekunde 
-    adrB <- AnfrageListe$adrB 
+    SessionStart <- as.Date(AnfrageListe$SessionStart) # Datum Stunde Sekunde
+    adrB <- AnfrageListe$adrB
     adrW <- AnfrageListe$adrW
     adrCol <- AnfrageListe$adrCol
     adrOut <- AnfrageListe$adrOut
     FBt <- AnfrageListe$FBt
-    
-    # Identifiziere die Items
-    # Rbonaut2::detectItemID(Stimulus = )
-    
-    
-    return(AnfrageListe)
-    
-  }
+
+    # Bereite Ergebnis vor
+    AnfrageDF <- data.frame(
+      TestID = AnfrageList$TestID,
+      idS = AnfrageList$idS,
+      idP = AnfrageList$idP,
+      NamePlayer = AnfrageList$NamePlayer,
+      SessionStart = as.POSIXlt(AnfrageList$SessionStart),
+      adrB = AnfrageList$adrB,
+      adrW = AnfrageList$adrW,
+      adrCol = AnfrageList$adrCol,
+      adrOut = AnfrageList$adrOut,
+      FBt = AnfrageList$FBt
+    )
+
+
+
+    return(AnfrageDF)
 }
 
 
 
 
 
- 
+
 
 
