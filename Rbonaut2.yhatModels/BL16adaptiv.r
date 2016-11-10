e92eed6a4fc70b37d57c0092f907ca17587fe0be
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2016-11-10 14:10:03 +0100

    BL16adaptiv überarbeitet.

--- Rbonaut2.yhatModels/BL16adaptiv.r
+++ Rbonaut2.yhatModels/BL16adaptiv.r
@@ -1,6 +1,4 @@
-rm(list=ls())
-library(Rbonaut2)
-library(jsonlite)
+# rm(list=ls())
 
 modelRequire <- function(){
   library(Rbonaut2)
@@ -13,40 +11,44 @@ modelTransform <- function(x){
 }
 
 modelPredict <- function(x){
-  #adaptiv.BL16.fullRandom.modelPredict(AnfrageDF=x)
-  return(jsonlite::toJSON(x))
+  Ergebnis <- adaptiv.BL16.fullRandom.modelPredict(AnfrageDF=x)
+  return(jsonlite::toJSON(Ergebnis))
 }
 
 
-###### Teststufe 1 ######  
+###### Teststufe 1 #############################################################################
 # Testung innerhalb einer .GlobalEnv in R
+#
+#
+#
+# AnfrageJSONstring <- jsonlite::toJSON(
+#   list(
+#     TestID = "BL32",
+#     idS = "002b6573-cf12-436d-bccd-0856b0bb0a25",
+#     idP = "fe553db4-bbde-43dd-a6a0-804b9e46c57",
+#     NamePlayer = "Mustermann, Tim",
+#     Birthday = "2002-07-21",
+#     Team = "U14",
+#     SessionStart = "2014-03-22 13:42:03",
+#     adrB = c(10, 45, 28),
+#     adrW = list(
+#               c(4, 6),
+#               c(21, 22),
+#               c(50, 18)
+#             ),
+#     adrCol = list(
+#                 c("A", "B"),
+#                 c("A", "B"),
+#                 c("B", "A")
+#               ),
+#     adrOut = c(4, 23, 40),
+#     FBt = c(2140, 2600, 8600)
+#   )
+# )
+#
+# # modelRequire()
+# AnfrageDF <- modelTransform(x = AnfrageJSONstring)
+# print(AnfrageDF)
+# modelPredict(AnfrageDF) # https://jsonformatter.curiousconcept.com : Output ist valides JSON
+#####################################################################################################
 
-
-
-Anfrage <- jsonlite::toJSON(
-  list(
-    TestID = "BL32",
-    idS = "002b6573-cf12-436d-bccd-0856b0bb0a25",
-    idP = "fe553db4-bbde-43dd-a6a0-804b9e46c57",
-    NamePlayer = "Mustermann, Tim",
-    Birthday = "2002-07-21",
-    Team = "U14",
-    SessionStart = "2014-03-22 13:42:03",
-    adrB = c(10, 45, 28),
-    adrW = list(
-              c(4, 6), 
-              c(21, 22), 
-              c(50, 18)
-            ),  
-    adrCol = list(
-                c("A", "B"), 
-                c("A", "B"), 
-                c("B", "A")
-              ),
-    adrOut = c(4, 23, 40),
-    FBt = c(2140, 2600, 8600)
-  )
-)
-
-# modelRequire()
-print(modelTransform(x = Anfrage))
