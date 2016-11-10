8746d181338d6406a81c20cb2581f4c1a0771516
HF on HF-iMac <harald.d.o.fiedler@gmail.com>
2016-11-10 08:53:41 +0100

    Python Code für Testlauf im LAN

---
+++ Rbonaut2.yHatCheck/1_lokaleMaschine/Test_yhat_local.py
@@ -0,0 +1,34 @@
+# pip install pycurl
+
+import pycurl
+import cStringIO
+import json
+from urllib import urlencode
+
+
+Modell = "test" # das zu testenden Model muss ~/models/test.r heissen (kleines r)
+
+
+data = {
+        "TestID" : "cs32",
+        "idS" : "002b6573-cf12-436d-bc45-0856b0bb0a77",
+        "idP" : "fe553db4-bbde-43dd-a6a0-804b9e46c57",
+        "NamePlayer" : "Mustermann, Tim",
+        "Birthday" : "2007-10-10",
+        "Team" : "U23",
+        "SessionStart" : "2014-03-22 13:42:03",
+        "adrB" : [64, 28, 10],
+        "adrW" : [68, 72, 22],
+        "adrOut" : [68, 72, 4],
+        "FBt" : [2140, 2600, 2300]
+    }
+
+anfrage = json.dumps(data)
+buf = cStringIO.StringIO()
+c = pycurl.Curl()
+c.setopt(pycurl.URL, 'localhost:5000/models/test')
+
+c.perform()
+c.close()
+print buf.getvalue()
+buf.close()
